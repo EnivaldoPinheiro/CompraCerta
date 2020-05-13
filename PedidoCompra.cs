1ed905a5d8675d5782687d6aa9aa0b638d904a24
PINHEIRO <56415884+EnivaldoPinheiro@users.noreply.github.com>
2020-05-12 21:28:45 -0300

    Add files via upload

---
+++ PedidoCompra.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompraCerta
+{
+    class PedidoCompra : Revendedora
+    {
+        protected string DadoDoVeiculo;
+        protected string DataPedido;
+        protected string DataEntrega;
+        protected double Valor;
+        protected double ValorFrete;
+        protected double ValorDesconto;
+        protected double ValorTotal;
+
+        public PedidoCompra(string carro,string dtpedido,string dtentrega,double preco,double frete,double desconto,double total)
+        {
+            DadoDoVeiculo = carro;
+            DataPedido = dtpedido;
+            DataEntrega = dtentrega;
+            Valor = preco;
+            ValorFrete = frete;
+            ValorDesconto = desconto;
+            ValorTotal = total;
+        }
+        public PedidoCompra()
+        {
+
+        }
+        public void veiculo(string carro)
+        {
+            DadoDoVeiculo = carro;
+        }
+        public string veiculo()
+        {
+            return DadoDoVeiculo;
+        }
+        public void dataPedido(string pedido)
+        {
+            DataPedido = pedido;
+        }
+        public string dataPedido()
+        {
+            return DataPedido;
+        }
+        public void dataEntrega(string entrega)
+        {
+            DataEntrega = entrega;
+        }
+        public string dataEntrega()
+        {
+            return DataEntrega;
+        }
+        public void Frete(double frete)
+        {
+            ValorFrete = frete;
+        }
+        public double Frete()
+        {
+            return ValorFrete;
+        }
+        public void Desconto(double desconto)
+        {
+            ValorDesconto = desconto;
+        }
+        public double Desconto()
+        {
+            return ValorDesconto;
+        }
+        public void Total(double total)
+        {
+            ValorTotal = total;
+        }
+        public double Total()
+        {
+            return ValorTotal;
+        }
+    }
+}

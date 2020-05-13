87ba7ca7fdef08cd23ab1f0e251f8a19a68bc8d1
PINHEIRO <56415884+EnivaldoPinheiro@users.noreply.github.com>
2020-05-12 21:29:21 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompraCerta
+{
+    static class Program
+    {
+
+        static void Main()
+        {
+
+            string Busca;
+           
+            PessoaFisica pf = new PessoaFisica();
+            pf.NomeFisico("Pinheiro");
+            Console.WriteLine();
+            Revendedora carro = new Revendedora();
+            Console.WriteLine("Informe o modelo do carro? ");
+            Busca = Console.ReadLine();
+            carro.TipoModelo(Busca);
+            Console.WriteLine();
+            Console.WriteLine("Informe a marca do carro? ");
+            Busca = Console.ReadLine();
+            carro.Fabricante(Busca);
+            Console.WriteLine();
+            Console.WriteLine("Informe a cor do carro? ");
+            Busca = Console.ReadLine();
+            carro.CorRgb(Busca);
+            Console.WriteLine();
+            Console.WriteLine("Informe o tipo do motor? ");
+            Busca= Console.ReadLine();
+            carro.TipoMotor(Busca);
+            Console.WriteLine();
+            Console.WriteLine("Informe o ano do carro? " + carro.Ano());
+            Busca = Convert.ToString(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Informe a media de consumo ? "+ carro.Litro_Km());
+            Busca = Convert.ToString(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Informe tipo de cambio? " + carro.cambio());
+            Busca = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Informe tipo de tração? " + carro.tracao());
+            Busca = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Informe tipo de cambio? " + carro.cambio());
+            Busca = Console.ReadLine();
+
+        }
+    }
+}
